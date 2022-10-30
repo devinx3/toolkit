941072e8e7e42836e2799815ed8175b799812ff4
devinx3 <devin@163.com>
2022-10-30 22:07:04 +0800

    [IMP]调整主页展示

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,52 +1,28 @@
 import React from 'react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
-
-const source = `
-# 工具箱
-
-收集常用的工具箱功能
-
-## 文本处理
-
-
-### 加密/解密
-
-**功能:** 对文本数据进行加密
-
-### 在线粘贴板(外链)
-
-[Note.ms](https://note.ms) 极简的粘贴板
-
-[文本派 - 极简的在线文本存储服务](https://txtpad.cn/)  支持密码, 多文本
-
-[网络剪贴板 - 极简的跨平台数据暂存和传送工具](https://netcut.cn/) 支持密码, 有效期, 文件
-
-[PrivateBin](https://bin.appinn.net/)  支持密码, 有效期, 文件, 阅后即焚, 讨论
-
-## JSON
-
-### 编辑
-
-**功能:** 格式化, 压缩, 删除字段值为空的值, 字段下划线转驼峰, 自定义处理方法(支持生成配置)
-
-### 展示(外链)
-
-[FeHelper - JSON美化工具](https://www.baidufe.com/fehelper/json-format/index.html)
-
-[JSON Crack](https://jsoncrack.com/editor)  视图展示JSON
-
-
-
-## 数据转换
-
-### 外链
-
-[FeHelper - Excel/CVS 转 JSON ](https://www.baidufe.com/fehelper/index/index.html) 支持转为 JSON, Xml, MySQL, Html
-
-[JAVA代码生成平台](http://java.bejson.com/generator/) SQL转换Java代码(MVC模式), 前端UI, Json, Xml
-`;
+import ReadMeSource from '../../README.md'
 
 const Home = () => {
-    return (<MarkdownPreview source={source} />);
+    const [source, setSource] = React.useState();
+
+    React.useEffect(() => {
+        if (source) {
+            return;
+        }
+        fetch(ReadMeSource)
+        .then(res => res.text())
+        .then(content => setSource(content))
+        .catch(e => console.error("fetch readme", e))
+    }, [source]);
+
+    return (<MarkdownPreview 
+        source={source} 
+        linkTarget='_blank'
+        rehypeRewrite={(node, index, parent) => {
+            if (node.tagName === "a" && parent && /^h(1|2|3|4|5|6)/.test(parent.tagName)) {
+              parent.children = parent.children.slice(1)
+            }
+        }}
+    />);
 }
 export default Home;
