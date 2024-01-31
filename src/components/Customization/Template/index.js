45096b1c683886329fdef48a2d9824cb60b6cd0d
devinx3 <devinx3@163.com>
2024-01-31 19:56:34 +0800

    [FIX] 渲染报错优化

--- src/components/Customization/Template/index.js
+++ src/components/Customization/Template/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Alert, Col, Row, Tabs } from 'antd';
+import { Typography, Alert, Col, Row, Tabs, Result, Popover, Button } from 'antd';
 import Converter from '../../common/Converter';
 import { ScriptResult } from '../../common/Converter/adaptor/index';
 import { LANG } from '../../common/Converter/constants';
@@ -13,62 +13,6 @@ const convertOuput = (outputData) => {
     let outputEle = outputData instanceof ScriptResult ? outputData.get() : outputData;
     if (!React.isValidElement(outputEle) && !(outputEle instanceof Function)) outputEle = undefined;
     return outputEle;
-}
-
-const needUpdate = (items1, items2) => {
-    const flag = items1.length !== items2.length;
-    if (flag) {
-        return true;
-    }
-    for (let idx = 0; idx < items1.length; idx++) {
-        const item1 = items1[idx];
-        const item2 = items2[idx];
-        if (item1.key !== item2.key) return true;
-        if (item1.label !== item2.label) return true;
-        if (item1.children !== item2.children) return true;
-    }    
-    return false;
-}
-
-// 选项卡
-const TabPanel = ({items }) => {
-    const [currentItems, setCurrentItems] = React.useState(() => [...items]);
-    const [currentKey, setCurrentKey] = React.useState();
-    if (needUpdate(currentItems, items)) {
-        setCurrentItems([...items])
-        setCurrentKey(items[items.length - 1].key)
-    }
-    const handleRemove = (k) => {
-        for (let idx = 0; idx < items.length; idx++) {
-            const item = items[idx];
-            if (item.key === k) {
-                items.splice(idx, 1)
-                break;
-            }
-        }
-        setCurrentItems([])
-    }
-    return <Tabs type="editable-card" animated={true} hideAdd={true} size='small'  tabBarGutter={0} activeKey={currentKey} items={currentItems} onChange={setCurrentKey} onEdit={handleRemove} />;
-}
-
-// 数据块渲染
-const DataBlockRender = ({ state, context, manageBlock, getCurrentNode }) => {
-    const { outputData, errorMsg } = state;
-    let currentNode = getCurrentNode();
-    const OutputEle = StrUtil.isBlank(errorMsg) ? convertOuput(outputData) : [];
-    let alertMessage = StrUtil.isBlank(errorMsg) && !OutputEle ? "" : errorMsg;
-    let children = null;
-    if (StrUtil.isBlank(alertMessage)) {
-        children = (OutputEle instanceof Function ? React.createElement(OutputEle) : (OutputEle ? OutputEle : null));
-        if (!children) {
-            children = !outputData ? manageBlock : <>{manageBlock}<div style={{ margin: '15px'}}><Row><Col span={8}><Alert message={"无渲染组件"} type="error" /></Col></Row></div></>;
-            currentNode = null;
-        }
-    } else {
-        children = <>{manageBlock}<div style={{ margin: '15px' }}><Row><Col><Alert message={<pre style={{marginBottom: 0, minWidth:"28vw"}}>{alertMessage}</pre>} type="error" /></Col></Row></div></>;
-        currentNode = null;
-    }
-    return <TabPanel updateItem={!!currentNode} items={getItems(context.category, currentNode, children, manageBlock)} />
 }
 
 // 获取选项卡内容
@@ -111,6 +55,90 @@ const getItems = (() => {
         return _items;
     }
 })();
+// 是否更新选项卡
+const needUpdate = (items1, items2) => {
+    const flag = items1.length !== items2.length;
+    if (flag) {
+        return true;
+    }
+    for (let idx = 0; idx < items1.length; idx++) {
+        const item1 = items1[idx];
+        const item2 = items2[idx];
+        if (item1.key !== item2.key) return true;
+        if (item1.label !== item2.label) return true;
+        if (item1.children !== item2.children) return true;
+    }
+    return false;
+}
+// 选项卡
+const TabPanel = ({ items }) => {
+    const [currentItems, setCurrentItems] = React.useState(() => [...items]);
+    const [currentKey, setCurrentKey] = React.useState();
+    if (needUpdate(currentItems, items)) {
+        setCurrentItems([...items])
+        setCurrentKey(items[items.length - 1].key)
+    }
+    const handleRemove = (k) => {
+        for (let idx = 0; idx < items.length; idx++) {
+            const item = items[idx];
+            if (item.key === k) {
+                items.splice(idx, 1)
+                break;
+            }
+        }
+        setCurrentItems([])
+    }
+    return <Tabs type="editable-card" animated={true} hideAdd={true} size='small' tabBarGutter={0} activeKey={currentKey} items={currentItems} onChange={setCurrentKey} onEdit={handleRemove} />;
+}
+
+class ScriptErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { didCatch: false };
+        this.printed = false;
+    }
+    static getDerivedStateFromError(error) {
+        return { didCatch: true, error };
+    }
+    getErrorMsg() {
+        if (!this.state.error) return '';
+        return this.state.error.message;
+    }
+    componentDidCatch(error, info) {
+        console.debug(`[${this.props.nodeName || '-'}]: 错误详情\n`, error);
+    }
+    render() {
+        if (this.state.didCatch) {
+            return <Result status='error' title="很抱歉, 脚本执行中发生了异常"
+                subTitle={<Popover title={this.getErrorMsg()} placement="bottomLeft" trigger='hover'>
+                    <Button>详情</Button>
+                </Popover>}>
+            </Result>
+        }
+        return this.props.children;
+    }
+}
+
+// 数据块渲染
+const DataBlockRender = ({ state, context, manageBlock, getCurrentNode }) => {
+    const { outputData, errorMsg } = state;
+    let currentNode = getCurrentNode();
+    const OutputEle = StrUtil.isBlank(errorMsg) ? convertOuput(outputData) : [];
+    let alertMessage = StrUtil.isBlank(errorMsg) && !OutputEle ? "" : errorMsg;
+    let children = null;
+    if (StrUtil.isBlank(alertMessage)) {
+        if (OutputEle) {
+            children = <ScriptErrorBoundary nodeName={currentNode?.name}>{OutputEle instanceof Function ? React.createElement(OutputEle) : OutputEle}</ScriptErrorBoundary>;
+        } else {
+            children = !outputData ? manageBlock : <>{manageBlock}<div style={{ margin: '15px' }}><Row><Col span={8}><Alert message={"无渲染组件"} type="error" /></Col></Row></div></>;
+            currentNode = null;
+        }
+    } else {
+        children = <>{manageBlock}<div style={{ margin: '15px' }}><Row><Col><Alert message={<pre style={{ marginBottom: 0, minWidth: "28vw" }}>{alertMessage}</pre>} type="error" /></Col></Row></div></>;
+        currentNode = null;
+    }
+    return <TabPanel updateItem={!!currentNode} items={getItems(context.category, currentNode, children, manageBlock)} />
+}
 
 // 模板页面
 const TemplatePage = ({ category, name }) => {
