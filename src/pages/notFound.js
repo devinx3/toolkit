aa83eef797ff9c1be23892f87f044fb4c5c99d34
devinx3 <devinx3@163.com>
2026-05-25 12:44:01 +0800

    fix: 修复地址问题

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Button, Result } from 'antd';
+import { useHistory } from 'react-router-dom';
 
-const NotFound = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="抱歉，你访问的页面不存在"
-    extra={<Button type="primary" onClick={e => window.location.hash='/'}>返回主页</Button>}
-  />
-);
+const NotFound = () => {
+  const history = useHistory();
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，你访问的页面不存在"
+      extra={<Button type="primary" onClick={e => history.push('/')}>返回主页</Button>}
+    />
+  );
+};
 
 export default NotFound;
